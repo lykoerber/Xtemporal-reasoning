f1032406dac0fe9c010c2a1e460fe1ab493a9d7d
LydiaKoerber <lydi.koerber@web.de>
2024-02-16 14:48:38 +0100

    adjust download script

--- download-data.sh
+++ download-data.sh
@@ -2,18 +2,24 @@
 mkdir data
 cd data
 
-# ambiguity resolution
-mkdir -p ambiguity_resolution
-cd ambiguity_resolution
-wget -c https://github.com/EternityYW/TRAM-Benchmark/raw/main/datasets/ambiguity_resolution.zip
-unzip -q ambiguity_resolution.zip
-rm ambiguity_resolution.zip
-cd ..
+subcorpora=("ambiguity_resolution" \
+            "arithmetic" \
+            "causality" \
+            "duration" \
+            "frequency" \
+            "nli_mcq" \
+            "nli_saq" \
+            "ordering" \
+            "relation" \
+            "storytelling" \
+            "typical_time")
 
-# duration
-mkdir duration
-cd duration
-wget -c https://github.com/EternityYW/TRAM-Benchmark/raw/main/datasets/duration.zip
-unzip -q duration.zip
-rm duration.zip
-cd ..
+# Loop through subcorpora
+for corpus in "${subcorpora[@]}"; do
+    mkdir -p "$corpus"
+    cd "$corpus" || exit
+    wget -c "https://github.com/EternityYW/TRAM-Benchmark/raw/main/datasets/$corpus.zip"
+    unzip -q "$corpus.zip"
+    rm "$corpus.zip"
+    cd ..
+done
---
+++ src/prompting.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import pandas as pd
+
+
+def read_data(dir):
+    df = pd.read_csv(data_path)
+
+
+def create_prompt(data):
+    pass
