0002e438245ca2a6c8cff26cf6f430b5f9c37823
LydiaKoerber <lydi.koerber@web.de>
2024-02-16 12:13:43 +0100

    adjust scripts

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=outputs.txt
 #SBATCH --mail-user=koerber@cl.uni-heidelberg.de
 #SBATCH --mail-type=ALL
-#SBATCH --time=00:20:00
+#SBATCH --time=00:10:00
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:1
 #SBATCH --partition=students
 
-srun python3 Xtemporal-reasoning/test_model.py
+srun python3 test_model.py
--- test_model.py
+++ test_model.py
@@ -1,27 +1,47 @@
+#!/usr/bin/env python
 import accelerate
-from transformers import LlamaConfig, LlamaTokenizer, LlamaForCausalLM
+from transformers import LlamaConfig, LlamaTokenizer, LlamaForCausalLM, BitsAndBytesConfig
+from time import time
+import torch
 
-
+t1 = time()
+print(t1)
 # Model names: "chrisyuan45/TimeLlama-7b-chat", "chrisyuan45/TimeLlama-13b-chat"
 model_name = "chrisyuan45/TimeLlama-7b-chat"
+quantization_config = BitsAndBytesConfig.from_dict({
+    'load_in_4bit': True,
+    'bnb_4bit_compute_dtype': torch.float16,
+    'bnb_4bit_quant_type': 'nf4',
+    'bnb_4bit_use_double_quant':True})
+
 model = LlamaForCausalLM.from_pretrained(
         model_name,
         return_dict=True,
-        load_in_8bit=quantization,
+        #load_in_8bit=True,
+        quantization_config = quantization_config,
         device_map="auto",
         low_cpu_mem_usage=True)
+t2 = time()
+print(f"model loaded: {t2}")
 tokenizer = LlamaTokenizer.from_pretrained(model_name)
-
+t3 = time()
+print(f"tokenizer loaded: {t3}")
 
 def generate(model, tokenizer, prompt):
-    inputs = tokenizer(prompt, return_tensors="pt")
+    inputs = tokenizer.encode(prompt, return_tensors="pt")
     # inputs.input_ids = inputs.input_ids.to('meta')
-    ids = model.generate(inputs.input_ids, max_length=30)
-    output = tokenizer.batch_decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    ids = model.generate(inputs, max_length=50, num_return_sequences=3, top_k=50)
+    output = [tokenizer.decode(ids[i], skip_special_tokens=True) for i in range(len(ids))]
     print(ids, output)
+    t4 = time()
+    print(f"generated: {t4}")
     return output
 
 
 if __name__=='__main__':
-    prompt = "How long did Cannes Film Festival 2019 last?"  # ,12 days,Facts
+    prompt = "How long did Cannes Film Festival 2019 last?\n"  # ,12 days,Facts
     generate(model, tokenizer, prompt)
+    prompt2 = "Pourquoi tu marches pas en fait?\n"
+    generate(model, tokenizer, prompt2)
+    prompt3 = "Hi! How are you?"
+    generate(model, tokenizer, prompt3)
